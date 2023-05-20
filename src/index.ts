544cc67f602d40b6cece9089d6087e821e642efd
Alexander Hyman <alexander.hyman05@gmail.com>
2023-05-19 17:37:44 -0700

    fix: / endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
-import Fastify from "fastify";
-import { JsonSchemaToTsProvider } from "@fastify/type-provider-json-schema-to-ts"
-import { config } from "dotenv";
-import { phone } from "./routes/phone";
-import { spotify } from "./routes/spotify"
+import Fastify from 'fastify';
+import { JsonSchemaToTsProvider } from '@fastify/type-provider-json-schema-to-ts';
+import { config } from 'dotenv';
+import { phone } from './routes/phone';
+import { spotify } from './routes/spotify';
 
-config()
+config();
 
 const app = Fastify().withTypeProvider<JsonSchemaToTsProvider>();
 const port = parseInt(process.env.PORT) || 3333;
 
-app.register(phone , { prefix: "/phone" });
-app.register(spotify, { prefix: "/spotify" });
+app.get('/', (request, reply) => {
+  reply
+    .code(200)
+    .header('Content-Type', 'application/json')
+    .send({
+      success: true,
+      message: "Welcome to Alexander's API! Specify an endpoint to get started."
+    });
+});
+
+app.register(phone, { prefix: '/phone' });
+app.register(spotify, { prefix: '/spotify' });
 
 (async () => {
   try {
@@ -18,7 +28,7 @@ app.register(spotify, { prefix: "/spotify" });
 
     console.log(`Server is now listening on ${port}`);
   } catch (error) {
-    console.error(error)
-    process.exit(1)
-  }  
-})()
+    console.error(error);
+    process.exit(1);
+  }
+})();
