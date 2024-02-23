a042499d03f66f3c7ce7f7a6b81205e5d32f4bb4
Lavender <alexander.hyman05@gmail.com>
2024-02-23 03:40:18 -0800

    fix: nuke prisma

--- prisma/migrations/20230909025424_autoincrement/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-CREATE SEQUENCE request_id_seq;
-ALTER TABLE "Request" ALTER COLUMN "id" SET DEFAULT nextval('request_id_seq');
-ALTER SEQUENCE request_id_seq OWNED BY "Request"."id";
